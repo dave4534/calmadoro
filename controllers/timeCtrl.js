abe2520ed19708ce037f4a479326ee0964528814
maitabe <martushb@gmail.com>
2016-12-06 21:16:11 +0200

    counter down

--- controllers/timeCtrl.js
+++ controllers/timeCtrl.js
@@ -1,5 +1,22 @@
-app.controller('TimeCtrl', function($scope){
+app.controller('TimeCtrl', function($scope, $timeout){
 
 	$scope.timer = 'we are on board';
+	//display timer on 25 min
+	$scope.counter = 25;
+	var stopped;
+
+	$scope.startTimer = function() {
+	    stopped = $timeout(function() {
+	       console.log($scope.counter);
+	     $scope.counter--;
+	     	console.log($scope.counter);
+	     $scope.startTimer();
+    }, 1000);
+  };
+
+  $scope.stopTimer = function(){
+   $timeout.cancel(stopped);
+
+    };
 
 });
