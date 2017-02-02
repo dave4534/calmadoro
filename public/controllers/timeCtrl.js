1d15ccaaa8e9437e611fdb17f14aaa878b2674a7
benjkn <benj.kn@gmail.com>
2017-02-02 14:36:13 +0200

    worked on after-countdown-life (p.s. there is life after countdown is over!!!!)

--- public/controllers/timeCtrl.js
+++ public/controllers/timeCtrl.js
@@ -2,15 +2,18 @@ app.controller('TimeCtrl', function($scope, $timeout){
 
   var selectedStyle = 'background-color: rgba(255, 255, 255, 0.25); color: white;';
   var notSelectedStyle = 'font-weight: normal';
-  $scope.togglingButton = 'START';
+
+  //initial conditions
   $scope.mode = 'Work';
   var seconds = 1500;
-  $scope.wstyle = selectedStyle;
+  $scope.togglingButton = 'START';
   $scope.bstyle = notSelectedStyle;
-  $scope.spotStyle = notSelectedStyle;
-  $scope.youStyle = selectedStyle;
+  $scope.wstyle = selectedStyle;
+
   var isTimerRunning = false;
   $scope.customMin = false;
+  // $scope.spotStyle = notSelectedStyle;
+  // $scope.youStyle = selectedStyle;
   // $scope.onoff = 'OFF';
   // var bind = 'off';
 
@@ -23,38 +26,41 @@ app.controller('TimeCtrl', function($scope, $timeout){
     if (setSeconds) {
       if ($scope.mode === 'Work') {
         $scope.workCustom = setSeconds;
-        // setSeconds.$setPristine();
+        // setSeconds.$setPristine();  <---- WHATS THIS???
       } else {
         $scope.breakCustom = setSeconds;
       }
       seconds = setSeconds;
       $scope.counterSec = getUItime(seconds);
+        //start the countdown when user chooses the minutes for the timer (no need to click Start!!)
+        if (!isTimerRunning) {
           $scope.startStop();
+        }
     } else {
+      //if user clicks 'Custom' show/hide the option to add an input
       $scope.customMin = !$scope.customMin;
     }
-
   };
 
 
   var getUItime = function(seconds) {
-  // multiply by 1000 because Date() requires miliseconds
-      var date = new Date(seconds * 1000);
-      var hh = date.getUTCHours();
-      var mm = date.getUTCMinutes();
-      var ss = date.getSeconds();
-      // If you were building a timestamp instead of a duration, you would uncomment the following line to get 12-hour (not 24) time
-      // if (hh > 12) {hh = hh % 12;}
-      // These lines ensure you have two-digits
-      if (hh < 10) {hh = "0"+ hh;}
-      if (mm < 10) {mm = "0"+ mm;}
-      if (ss < 10) {ss = "0"+ ss;}
-      // This formats your string to HH:MM:SS
-      if (hh>0) {
-        var result = hh+":"+mm+":"+ss;
-      } else {
-        var result = mm + ":" + ss;
-      }
+    // multiply by 1000 because Date() requires miliseconds
+    var date = new Date(seconds * 1000);
+    var hh = date.getUTCHours();
+    var mm = date.getUTCMinutes();
+    var ss = date.getSeconds();
+    // If you were building a timestamp instead of a duration, you would uncomment the following line to get 12-hour (not 24) time
+    // if (hh > 12) {hh = hh % 12;}
+    // These lines ensure you have two-digits
+    if (hh < 10) {hh = "0"+ hh;}
+    if (mm < 10) {mm = "0"+ mm;}
+    if (ss < 10) {ss = "0"+ ss;}
+    // This formats your string to HH:MM:SS
+    if (hh>0) {
+      var result = hh + ":" + mm + ":" + ss;
+    } else {
+      var result = mm + ":" + ss;
+    }
     return result;
   };
 
@@ -74,6 +80,12 @@ app.controller('TimeCtrl', function($scope, $timeout){
           isTimerRunning = !isTimerRunning;
           $scope.startStop();
         } else {
+          if ($scope.mode === 'Work') {
+            $scope.modeWorkBreak('Break')
+          } else {
+            $scope.modeWorkBreak('Work')
+          }
+          $scope.customMin = false;
           alertMe();
         }
       }, 1000);
@@ -87,21 +99,21 @@ app.controller('TimeCtrl', function($scope, $timeout){
   $scope.resetTimer = function() {
 
     if ($scope.mode === 'Work') {
-        if ($scope.workCustom) { $scope.setTimeTo($scope.workCustom); }
-        else { $scope.setTimeTo(1500);}
+      if ($scope.workCustom) { $scope.setTimeTo($scope.workCustom); }
+      else { $scope.setTimeTo(1500) }
 
-        //stop the timer if it is running
-        if (isTimerRunning) { $scope.startStop(); }
+      //stop the timer if it is running
+      if (isTimerRunning) { $scope.startStop(); }
 
     } else if ($scope.mode === 'Break') {
-        if ($scope.breakCustom) { $scope.setTimeTo($scope.breakCustom); }
-        else {$scope.setTimeTo(300);}
+      if ($scope.breakCustom) { $scope.setTimeTo($scope.breakCustom); }
+      else { $scope.setTimeTo(300) }
 
-        //stop the timer if it is running
-        if (isTimerRunning) { $scope.startStop(); }
+      //stop the timer if it is running
+      if (isTimerRunning) { $scope.startStop(); }
 
     } else {
-        console.log ('error');
+      console.log ('error');
     }
 
   };
@@ -129,6 +141,11 @@ app.controller('TimeCtrl', function($scope, $timeout){
   };
 
 
+  var alertMe = function() {
+    var audio = new Audio('audio/buzzer.mp3');
+    audio.play();
+  };
+
   // **********NOT YET**********
   // $scope.bindingBtn = function () {
   //   if (bind === 'off') {
@@ -141,23 +158,18 @@ app.controller('TimeCtrl', function($scope, $timeout){
   // };
 
 
-  $scope.musicstyle = function (musicSource) {
-    if (musicSource === 'Spotify') {
-      $scope.spotStyle = selectedStyle;
-      $scope.youStyle = notSelectedStyle;
-    } else if (musicSource === 'Youtube') {
-      $scope.spotStyle = notSelectedStyle;
-      $scope.youStyle = selectedStyle;
-    } else {
-      console.log ('error in the music source');
-      console.log (musicSource);
-    }
-  };
+  // $scope.musicstyle = function (musicSource) {
+  //   if (musicSource === 'Spotify') {
+  //     $scope.spotStyle = selectedStyle;
+  //     $scope.youStyle = notSelectedStyle;
+  //   } else if (musicSource === 'Youtube') {
+  //     $scope.spotStyle = notSelectedStyle;
+  //     $scope.youStyle = selectedStyle;
+  //   } else {
+  //     console.log ('error in the music source');
+  //     console.log (musicSource);
+  //   }
+  // };
 
-
-  var alertMe = function() {
-    var audio = new Audio('audio/buzzer.mp3');
-    audio.play();
-  };
 
 });
